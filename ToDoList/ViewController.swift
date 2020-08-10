a4942527409f745f6924a4221fbbda14d037e6df
Julia Chang <juliayungchang@gmail.com>
2020-08-10 13:11:52 -0700

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Julia Chang  on 8/10/20.
+//  Copyright © 2020 Julia Chang . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
